2e67f263cf56cdc6e4d7388c40adfe45187c89af
zer0231 <pranay_zer0@protonmail.com>
2023-03-21 20:26:54 +0545

    Navigation drawer created

--- app/src/main/java/com/zero/drivesafe/MainActivity.java
+++ app/src/main/java/com/zero/drivesafe/MainActivity.java
@@ -1,14 +1,27 @@
 package com.zero.drivesafe;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    DrawerLayout drawerLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Adding toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        drawerLayout = findViewById(R.id.draw_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close); //the string is added so that accessibility can read it loud
+//        drawerLayout.addDrawerListener(toggle);
+//        toggle.syncState();
+
     }
 }
